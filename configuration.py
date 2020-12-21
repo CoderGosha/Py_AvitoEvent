03238582a905c14495134680f4cdb76bb66abe05
CoderGosha <coder.gosha@gmail.com>
2020-12-21 18:02:03 +0300

    Добавлена настройка названия конфига для локальной отладки

--- configuration.py
+++ configuration.py
@@ -7,7 +7,7 @@ class Configuration:
 
     def __init__(self):
         file_dir = os.path.abspath(__file__).replace(__name__ + ".py", "")
-        filename = os.path.join(file_dir, "static_files", "config.json")
+        filename = os.path.join(file_dir, "static_files", os.getenv('config_json_name', "config.json"))
 
         with open(filename, 'r') as f:
             self.config = json.load(f)
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 Конфигурация бота в config.json
-
+Так же можно переопределить переменную окружения для конфига config_json_name
 Прокси сервер без логинов
 docker run -d --name socks5 -p 1081:1080 serjs/go-socks5-proxy
